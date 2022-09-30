4617150e813cd8c3a85d74950a00acf540ae0110
Antti Tiikkala <antti.tiikkala@hotmail.com>
2022-09-30 14:43:57 +0300

    "Harjoitus tavoitesykkeen palautus"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+const [Age, setAge] = useState('')
+const [Upper, setUpper] = useState(0)
+const [Lower, setLower] = useState(0)
+
+
+
+
+function laske(e) {
+  e.preventDefault() //Estää refreshauksen
+  const Upper = (220 - Age) * 0.85
+  const Lower = (220 - Age) * 0.65
+  setUpper(Upper)
+  setLower(Lower)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="content">
+      <h3>Heart rate limits calculator</h3>
+      <form onSubmit={laske}>
+        <div>
+          <label>Age</label>
+          <div>
+          <input value={Age} onChange={e => setAge(e.target.value)}/>
+        </div>
+        </div>
+        <div>
+          <label>Heart rate limits</label>
+          <div>
+          <output>{Lower.toFixed(0)}-{Upper.toFixed(0)}</output>
+        </div>
+        </div>
+        <button>Calculate</button>
+      </form>
     </div>
+
   );
 }
 
